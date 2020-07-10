954407c16c41817e05d2c6f229177cee0470ea27
Nroulston <nicolas.roulston@gmail.com>
2020-07-10 13:43:16 -0700

    brew log views added, brew long controller made, got tux working

--- Gemfile
+++ Gemfile
@@ -1,14 +1,16 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
-gem 'activerecord', '~> 4.2', '>= 4.2.6', :require => 'active_record'
+gem 'activerecord', '5.2.3', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+
 gem 'rake'
 gem 'require_all'
 gem 'sqlite3', '~> 1.3.6'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
+gem 'pry-nav'
 gem 'bcrypt'
 gem 'tux'
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,49 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200710200213) do
+ActiveRecord::Schema.define(version: 2020_07_10_200213) do
 
   create_table "brew_logs", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.string   "method"
-    t.string   "boil_time"
-    t.string   "batch_size"
-    t.string   "pre_boil_size"
-    t.string   "pre_boil_gravity"
-    t.string   "target_fg"
-    t.string   "target_og"
-    t.string   "ibu"
-    t.string   "srm"
-    t.string   "mash_ph"
-    t.string   "mash_schedule"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.integer "user_id"
+    t.string "name"
+    t.string "method"
+    t.string "boil_time"
+    t.string "batch_size"
+    t.string "pre_boil_size"
+    t.string "pre_boil_gravity"
+    t.string "target_fg"
+    t.string "target_og"
+    t.string "ibu"
+    t.string "srm"
+    t.string "mash_ph"
+    t.string "mash_schedule"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.string   "method"
-    t.string   "boil_time"
-    t.string   "batch_size"
-    t.string   "pre_boil_size"
-    t.string   "pre_boil_gravity"
-    t.string   "target_fg"
-    t.string   "target_og"
-    t.string   "ibu"
-    t.string   "srm"
-    t.string   "mash_ph"
-    t.string   "mash_schedule"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.integer "user_id"
+    t.string "name"
+    t.string "method"
+    t.string "boil_time"
+    t.string "batch_size"
+    t.string "pre_boil_size"
+    t.string "pre_boil_gravity"
+    t.string "target_fg"
+    t.string "target_og"
+    t.string "ibu"
+    t.string "srm"
+    t.string "mash_ph"
+    t.string "mash_schedule"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
