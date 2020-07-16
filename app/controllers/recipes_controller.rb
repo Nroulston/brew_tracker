4b6fddb60886bbb394f135b693fcc60ace562921
Nroulston <nicolas.roulston@gmail.com>
2020-07-16 12:15:44 -0700

    recipe hop table populated with rest of hop params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,13 +14,20 @@ class RecipesController < ApplicationController
   recipe.save
   #associate or create a hop takes on hop and make it. If you figure out jquery creation of form inputs then you need to change below to an each do block to create find or create each hop.
   
-  hop = Hops.find_or_create_by(name: params[:hop][0][:name], form: params[:hop][0][:form], alpha_acid: params[:hop][0][:alpha_acid])
+  hop = Hop.find_or_create_by(name: params[:hop][0][:name], form: params[:hop][0][:form], alpha_acid: params[:hop][0][:alpha_acid])
   recipe.hops << hop  
   join_hop = RecipeHop.last
-  # #associate or create time_added, measurement_amount
-   time = TimeAdded.find_or_create_by(time_added: params[:hop][0][:time_added])
-  #  measurement = Measurement.find_or_create_by(measurement: params[:hop][0][:measurement])
+  
+  time = TimeAdded.find_or_create_by(time_added: params[:hop][0][:time_added])
+  join_hop.time_added = time
 
+  measurement = Measurement.find_or_create_by(measurement: params[:hop][0][:measurement])
+  join_hop.measurement = measurement
+
+  measurement_amount = MeasurementAmount.find_or_create_by(measurement_amount: params[:hop][0][:measurement_amount])
+  join_hop.measurement_amount = measurement_amount
+  
+  join_hop.time_added
     binding.pry
     redirect "/recipes"
   end
@@ -31,6 +38,9 @@ class RecipesController < ApplicationController
 
   get "/recipes/:id/edit" do
 
+    #Use the below to access a recipes hops join table and also the associated attribute objects. 
+    #use recipe = whatever you need to find the current recipe
+    #recipe.recipe_hops.first.time_added 
 
     erb :"/recipes/edit.html"
   end
