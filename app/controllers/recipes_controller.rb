8870dc26073f5ec67c99f7a9282ede0aa4f97b36
Nroulston <nicolas.roulston@gmail.com>
2020-07-16 11:59:08 -0700

    changed hop find_or_create_by method to not build off the Recipe. Building off the recipe created a new hop each time even if the hop existed in the database.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,11 +13,13 @@ class RecipesController < ApplicationController
   recipe = Recipe.new(params[:recipe])
   recipe.save
   #associate or create a hop takes on hop and make it. If you figure out jquery creation of form inputs then you need to change below to an each do block to create find or create each hop.
-  recipe.hops.find_or_create_by(name: params[:hop][0][:name], form: params[:hop][0][:form], alpha_acid: params[:hop][0][:alpha_acid])
-    join_hop = RecipeHop.last
+  
+  hop = Hops.find_or_create_by(name: params[:hop][0][:name], form: params[:hop][0][:form], alpha_acid: params[:hop][0][:alpha_acid])
+  recipe.hops << hop  
+  join_hop = RecipeHop.last
   # #associate or create time_added, measurement_amount
    time = TimeAdded.find_or_create_by(time_added: params[:hop][0][:time_added])
-  #  measurement = Measurement.find_or_create_by(name: params[:hop][0][:measurement])
+  #  measurement = Measurement.find_or_create_by(measurement: params[:hop][0][:measurement])
 
     binding.pry
     redirect "/recipes"
