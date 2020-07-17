d150835ca5a5edd7f6bcabaccbba89354c383145
Nroulston <nicolas.roulston@gmail.com>
2020-07-17 11:50:46 -0700

    created hop patch logic, refactored new recipe hop logic to acccount for multiple hops. Changed helper methods to include index to work with patch method.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,32 +10,41 @@ class RecipesController < ApplicationController
 
   post "/recipes" do
   #create a new recipe
-  recipe = Recipe.new(params[:recipe])
-  recipe.save
+  @recipe = Recipe.new(params[:recipe])
+  @recipe.save
   
   # Todo If you figure out jquery creation of form inputs then you need to change below to an each do block to create find or create each ingredient.
  
   # Todo potentially use .send to make Hop.find_or_create_by dynamic for all ingredients def set_property(obj, prop_name, prop_value) obj.send("#{prop_name}=",prop_value) end potentially use hashes build out an attr accessor with hash of ingredients with classname, params accessors, join_tables, basically all things that might be variable per ingredient. This would allow you to make only one change to the ingredients hash to make changes to the entire process. 
   
-  hop = Hop.find_or_create_by(name: params[:hop][0][:name], form: params[:hop][0][:form], alpha_acid: params[:hop][0][:alpha_acid])
-  recipe.hops << hop 
-  join_hop = RecipeHop.last
-  add_time_measurement_and_measurement_quantity_to_join_table_record(join_hop, :hop)
   
-  fermentable = Fermentable.find_or_create_by(name: params[:fermentable][0][:name])
-  recipe.fermentables << fermentable
-  join_fermentable = RecipeFermentable.last
-  add_time_measurement_and_measurement_quantity_to_join_table_record(join_fermentable, :fermentable)
+  params[:hop].each.with_index do |hop_details, index|
+    hop_instance = Hop.find_or_create_by(name: hop_details[:name], form: hop_details[:form], alpha_acid: hop_details[:alpha_acid])
+    @recipe.hops << hop_instance 
+    join_hop = @recipe.recipe_hops.last
+    add_time_measurement_and_measurement_quantity_to_join_table_record(join_hop, index, :hop )
+  end
+
+  params[:fermentable].each.with_index do |grain_details, index|
+    fermentable = Fermentable.find_or_create_by(name: grain_details[:name])
+    @recipe.fermentables << fermentable
+    join_fermentable = @recipe.recipe_fermentables.last
+    add_time_measurement_and_measurement_quantity_to_join_table_record(join_fermentable, index, :fermentable)
+  end
   
-  yeast = Yeast.find_or_create_by(name: params[:yeast][0][:name])
-  recipe.yeasts << yeast
-  join_yeast = RecipeYeast.last
-  add_time_measurement_and_measurement_quantity_to_join_table_record(join_yeast, :yeast)
-  
-  other_ingredient = OtherIngredient.find_or_create_by(name: params[:other_ingredient][0][:name])
-  recipe.other_ingredients << other_ingredient
-  join_other_ingredient = RecipeOtherIngredient.last
-  add_time_measurement_and_measurement_quantity_to_join_table_record(join_other_ingredient, :other_ingredient)
+  params[:yeast].each.with_index do |yeast_details, index|
+    yeast = Yeast.find_or_create_by(name: yeast_details[:name])
+    @recipe.yeasts << yeast
+    join_yeast = @recipe.recipe_yeasts.last
+    add_time_measurement_and_measurement_quantity_to_join_table_record(join_yeast, index, :yeast)
+  end
+
+  params[:other_ingredient].each.with_index do |ingredient_details, index|
+    other_ingredient = OtherIngredient.find_or_create_by(name: params[:other_ingredient][0][:name])
+    @recipe.other_ingredients << other_ingredient
+    join_other_ingredient = @recipe.recipe_other_ingredients.last
+    add_time_measurement_and_measurement_quantity_to_join_table_record(join_other_ingredient, index,:other_ingredient)
+  end
 
 
   
@@ -57,9 +66,38 @@ end
 patch "/recipes/:id" do
   binding.pry
   set_recipe
-  table_hop = @recipe.recipe_hops
+  hops_count = @recipe.recipe_hops.count
+  @recipe.update(params[:recipe])
+  #Todo look up how to not run the next lines of code of the form submitted has no changes from it's initial value, or how to not run it for items that had no changes. Example hops didn't change at all, but extra ingredients changed the time they were added. 
+
+  #Todo see how to 
+  
+  index = 0
+  while index < hops_count
+    hop = Hop.find_or_create_by(name: params[:hop][index][:name], form: params[:hop][index][:form], alpha_acid: params[:hop][index][:alpha_acid])
+    join_hop = @recipe.recipe_hops[index]
+    join_hop.hop = hop
+    add_time_measurement_and_measurement_quantity_to_join_table_record(join_hop, :hop)
+    index += 1
+  end
+  
+  index = hops_count
+  while index < params[:hop].count
+    hop = Hop.find_or_create_by(name: params[:hop][index][:name], form: params[:hop][index][:form], alpha_acid: params[:hop][index][:alpha_acid])
+    @recipe.hops << hop
+    join_hop = @recipe.recipe_hops.last
+    add_time_measurement_and_measurement_quantity_to_join_table_record(join_hop, index, :hop)
+    index += 1
+  end
+
+  
+
+
+
+  
+
   #todo need to test if when editing a recipe with no hops that there is a recipe_hops join table that can be accessed before adding the new hop to the association.
-  redirect "/recipes/:id"
+  redirect "/recipes/#{@recipe.id}"
 end
 
 delete "/recipes/:id/delete" do
@@ -72,18 +110,18 @@ end
       @recipe = Recipe.find_by_id(params[:id])
     end
 
-    def add_time_measurement_and_measurement_quantity_to_join_table_record(join_table, ingredient)
-      time, measurement, measurement_amount = make_or_find_time_measurement_measurement_amount_from_params(ingredient)
+    def add_time_measurement_and_measurement_quantity_to_join_table_record(join_table, index, ingredient )
+      time, measurement, measurement_amount = make_or_find_time_measurement_measurement_amount_from_params(ingredient, index)
       join_table.time_added = time
       join_table.measurement = measurement
       join_table.measurement_amount = measurement_amount
       join_table.save
     end
 
-    def make_or_find_time_measurement_measurement_amount_from_params(ingredient)
-      time = TimeAdded.find_or_create_by(time_added: params[ingredient][0][:time_added])
-      measurement = Measurement.find_or_create_by(measurement: params[ingredient][0][:measurement])
-      measurement_amount = MeasurementAmount.find_or_create_by(measurement_amount: params[ingredient][0][:measurement_amount])
+    def make_or_find_time_measurement_measurement_amount_from_params(ingredient, index)
+      time = TimeAdded.find_or_create_by(time_added: params[ingredient][index][:time_added])
+      measurement = Measurement.find_or_create_by(measurement: params[ingredient][index][:measurement])
+      measurement_amount = MeasurementAmount.find_or_create_by(measurement_amount: params[ingredient][index][:measurement_amount])
       return time,measurement,measurement_amount
     end
   end
