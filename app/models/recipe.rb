ae97dc60d2b3ba110516f8c0d487e540821e3950
Nroulston <nicolas.roulston@gmail.com>
2020-07-28 19:19:27 -0700

    refactored code to a has many through relationship using the brew_log as a join table. Refactored code inside the brew_log index to reflect the new has many relationship.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -17,22 +17,3 @@ class Recipe < ActiveRecord::Base
   has_many :hops, through: :recipe_hops
 end
 
-#Todo need to use the following sql or something similar to add each foreign key into the Recipe_hop join table 
-# sql = <<-SQL
-# INSERT INTO recipe_hops (recipe_id, time_added_id, hop_id, created_at, updated_at) VALUES (3, 1, 1,datetime('now'),datetime('now'))
-# SQL
-
-# INSERT INTO datetime_text (d1, d2)
-# VALUES(datetime('now'),datetime('now', 'localtime'));
-# ActiveRecord::Base.connection.execute(sql)
-
-# class DeleteCreateAtAndUpdatedAtColumnFromRecipeHopsTable < ActiveRecord::Migration[5.2]
-#   def change
-
-#     remove_column :recipe_hops, :created_at
-#     remove_column :recipe_hops, :updated_at
-#   end
-# end
-
-
-# SELECT "measurement_qties".* FROM "measurement_qties" INNER JOIN "recipe_fermentables" ON "measurement_qties"."id" = "recipe_fermentables"."measurement_qty_id" WHERE "recipe_fermentables"."recipe_id" = ?  [["recipe_id", 1]]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true, uniqueness: true
-  validates :password, presence: true
-
+ 
+  
   has_many :recipes
   has_many :brew_logs
+
+  has_many :recipe_copies , through: :brew_logs, source: :recipe
+
 end
